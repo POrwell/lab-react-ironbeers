e396e032d3940daa2c3ce4977abd54022aeb13df
Phoebe Orwell <phoebe.orwell@gmail.com>
2022-11-18 15:57:48 +0100

    Iteration3 complete

--- src/pages/Beers.js
+++ src/pages/Beers.js
@@ -1,11 +1,30 @@
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
+import axios from "axios";
 
 function Beers() {
+const [beers, setBeers] = useState([])
+useEffect(() => {
+    const fetchBeers = async () => {
+        let response = await axios.get("https://ih-beers-api2.herokuapp.com/beers");
+        console.log(response.data)
+        setBeers(response.data)
+    }
+    fetchBeers();
+})
     return(
-<div>
+<>
 <Navbar/>
-</div>
+{beers.map((beer) =>
+<Link className="Link" key={beer._id} to="/beers/:beerId">
+<img className="Image" src={beer.image_url}/>
+<p>{beer.name}</p>
+<p>{beer.tagline}</p>
+<p>{beer.contributed_by}</p>
+</Link>
+)}
+</>
     )
 }
 
